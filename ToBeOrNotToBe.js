b643f96bba04a631c44276ca5a36cbaeed0d98c0
Nasir Hussain <95434256+nasirhussainn@users.noreply.github.com>
2023-12-14 21:48:53 +0500

    Create ToBeOrNotToBe.js

---
+++ ToBeOrNotToBe.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+     return {
+    toBe: function (otherVal) {
+      if (val !== otherVal) {
+        throw new Error("Not Equal");
+      }
+      return true;
+    },
+    notToBe: function (otherVal) {
+      if (val === otherVal) {
+        throw new Error("Equal");
+      }
+      return true;
+    }
+  };
+};
+
+try{
+ expect(5).toBe(5); // true
+ expect(5).notToBe(5); // throws "Equal"
+}catch(error){
+    console.log(error.message)
+}
+ 
